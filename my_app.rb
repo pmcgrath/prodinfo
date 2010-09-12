396e09ebc6b5d21c71c09d795059bddb54462605
pmcgrath <pmcgrath@gmail.com>
2010-09-12 23:15:51 +0100

    .

--- my_app.rb
+++ my_app.rb
@@ -3,20 +3,21 @@ require 'sinatra'
 
 
 configure :production do
-  # Configure stuff here you'll want to
-  # only be run at Heroku at boot
+  	# Configure stuff here you'll want to
+	# only be run at Heroku at boot
 
-  # TIP: You can get you database information
-  # from ENV['DATABASE_URI'] (see /env route below)
+  	# TIP: You can get you database information
+  	# from ENV['DATABASE_URI'] (see /env route below)
 end
 
 get '/' do
-  "We're in !"
+  	"We're in !"
 end
 
-
 get '/ted' do
-  "<html>"
-  "	<h1>Ted</h1>"
-  "</html>"
+	page_source = "<html>"
+	page_source += "	<h1>Ted</h1>"
+	page_source += "</html>"
+
+	page_source
 end
