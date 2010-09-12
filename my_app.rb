800d62ca3f0462cbd9439b9d577526cc2c98c577
pmcgrath <pmcgrath@gmail.com>
2010-09-13 00:38:42 +0100

    Detect client IP

--- my_app.rb
+++ my_app.rb
@@ -16,7 +16,7 @@ end
 
 get '/ted' do
 	page_source = "<html>"
-	page_source += "	<h1>Ted</h1>"
+	page_source += "	<h1>Your ip is #{@env['REMOTE_ADDR']}</h1>"
 	page_source += "</html>"
 
 	page_source
